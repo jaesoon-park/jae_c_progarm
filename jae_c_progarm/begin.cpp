f471ef0a6806d2b14832e622526837cdc2c5009b
jaesoon-park <wotns2606@cu.ac.kr>
2019-03-29 11:27:44 +0900

    struct

--- jae_c_progarm/begin.cpp
+++ jae_c_progarm/begin.cpp
@@ -1,9 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<string.h>
+
+struct employee {
+	char name[10];
+	int year;
+	int pay;
+};
+
+
 
 int main()
 {
+	struct employee Lee;
+	struct employee *Sptr = &Lee;
+	strcpy(Sptr->name, "이순신");
+	Sptr->year = 2015;
+	Sptr->pay = 5900;
 
-	printf("beginning_c");
 
+	printf("\n 이름 : %s", Sptr->name);
+	printf("\n 연봉 : %d", Sptr->year);
+	printf("\n 입사 : %d\n", Sptr->pay);
+	
 }
